fe68035ca2275976462a8fe99a9a08414644f78f
unknown <chengkai.2nd@gmail.com>
2022-02-12 11:32:47 -0600

    update comments

---
+++ sendEmail.py
@@ -0,0 +1,31 @@
+import os
+import smtplib
+from emailList import email_receivers
+
+
+class SendEmail:
+    def __init__(self, item_ID, price, url):
+
+        """
+        email address and password are save in os environment
+        """
+        self.email = os.environ.get("EMAIL_ADDRESS")
+        self.password = os.environ.get("EMAIL_PASSWORD")
+
+        self.item = item_ID
+        self.price = price
+        self.url = url
+
+    def Send(self):
+
+        """USe Gmail Server to send emails. Use the following url to set less secure Gmail access
+        https://myaccount.google.com/lesssecureapps"""
+        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login(self.email, self.password)
+            subject = f"{self.item} ${self.price} is back stock!"
+            msg = f"Subject: {subject} \n\n {self.url}"
+
+            smtp.sendmail(self.email, email_receivers, msg)
